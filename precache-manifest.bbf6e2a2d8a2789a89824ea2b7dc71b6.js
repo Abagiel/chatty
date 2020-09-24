c1bea2e29a55811f16d6740ae1bd79dc4641342d
Abagiel <66873762+Abagiel@users.noreply.github.com>
2020-09-24 21:17:42 +0300

    test

---
+++ precache-manifest.bbf6e2a2d8a2789a89824ea2b7dc71b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d83d044c4682e59f690208fd37c2c2",
+    "url": "/github.io/chatty/index.html"
+  },
+  {
+    "revision": "95a1c10c9a97e43fdbd1",
+    "url": "/github.io/chatty/static/css/main.5cee073c.chunk.css"
+  },
+  {
+    "revision": "c72efbf24ee9a588e9ef",
+    "url": "/github.io/chatty/static/js/2.5d44fcff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github.io/chatty/static/js/2.5d44fcff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a1c10c9a97e43fdbd1",
+    "url": "/github.io/chatty/static/js/main.3985347a.chunk.js"
+  },
+  {
+    "revision": "ccf1add4a585cd1a0542",
+    "url": "/github.io/chatty/static/js/runtime-main.1a868cb7.js"
+  }
+]);
